88dbf2a117d707fb3c2f249a0d3472204bb620f1
ppomaster <116190741+ppomaster@users.noreply.github.com>
2025-09-02 04:15:33 -0300

    Criar config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyBu6FyItXddK1pghqaEURKaMiQTMQZlJks",
+  authDomain: "colecaodefilmes-dfa7a.firebaseapp.com",
+  projectId: "colecaodefilmes-dfa7a",
+  storageBucket: "colecaodefilmes-dfa7a.appspot.com",
+  messagingSenderId: "495818238503",
+  appId: "1:495818238503:web:814178c233d5fb304a7c02"
+};
+
+export const OMDB_API_KEY = "6cbfb854";
